a96150437ca21d50a89b511b04f04287f4801a94
Le Anh Tuan <atctw.it@gmail.com>
2023-07-16 09:13:04 +0700

    test: detect client device info

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
+import * as DeviceDetector from 'node-device-detector';
+import * as ClientHints from 'node-device-detector/client-hints';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private deviceDetector;
+
+  constructor(private readonly appService: AppService) {
+    //@ts-ignore
+    this.deviceDetector = new DeviceDetector({
+      clientIndexes: true,
+      deviceIndexes: true,
+      deviceAliasCode: false,
+    });
+  }
 
   @Get('department')
   getListOfDepartments() {
@@ -18,5 +30,21 @@ export class AppController {
   @Get('attendance-status')
   getListOfAttendanceStatus() {
     return this.appService.getListOfAttendanceStatus();
+  }
+
+  @Get('device')
+  getClientDeviceInfo(@Req() req: Request) {
+    //@ts-ignore
+    const clientHints = new ClientHints();
+    const clientHintData = clientHints.parse(req.headers);
+
+    return {
+      noHint: this.deviceDetector.detect(req.headers['user-agent'] ?? ''),
+
+      withHint: this.deviceDetector.detect(
+        req.headers['user-agent'] ?? '',
+        clientHintData,
+      ),
+    };
   }
 }
